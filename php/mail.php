aef14f7a79ada258be893f3baa9dcd2e909b1014
kyzblop <jeremymoi.jd@gmail.com>
2024-10-04 16:06:08 +0200

    suppression php

--- php/mail.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$retour = mail('jeremymoi.jd@gmail.com', 'Mail depuis le CV en ligne', $_POST['message'], 'From : $POST['adresseEnvoie']');
-if(retour)
-    echo'Votre message est envoyé';
-
-?>
